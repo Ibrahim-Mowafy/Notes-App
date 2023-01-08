ec2ab531b61428faa1bec78c187e5ce0f6f2957a
FIRST_NAME LAST_NAME <ibrahim.mowafy10@gmail.com>
2023-01-08 18:29:36 +0200

    Adding backend code for submit form note

--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,7 +1,26 @@
+import { redirect } from '@remix-run/node';
+import NewNote, { links as NewNoteLinks } from '~/components/NewNote';
+import { getStoredNotes, storeNotes } from '~/data/notes';
+
 export default function NotesPage() {
   return (
     <main>
-      <h1>My Notes</h1>
+      <NewNote />
     </main>
   );
+}
+
+export async function action({ request }) {
+  const formData = await request.formData();
+  const noteData = Object.fromEntries(formData);
+  //  Add validation...
+  const existingNotes = await getStoredNotes();
+  noteData.id = new Date().toISOString();
+  const updatedNotes = existingNotes.concat(noteData);
+  await storeNotes(updatedNotes);
+  return redirect('/notes');
+}
+
+export function links() {
+  return [...NewNoteLinks()];
 }
